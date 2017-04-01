5123709fc2b49b073b32f6dd3eec420ce4d1e67e
dsmith464 <dsmith464@gatech.edu>
2017-04-01 05:11:36 -0500

    Added index.php

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Bare - Start Bootstrap Template</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Custom CSS -->
+    <style>
+    body {
+        padding-top: 70px;
+        /* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
+    }
+    </style>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    
+
+</head>
+
+<body>
+
+	<div id="body"></div>
+    <hr>
+    <div class="container">
+        <label>What would you like?</label>
+        <input id="question" type="text" class="form-control" />
+        <a id="ask-submit"  class="btn btn-default">Submit</a>
+    </div>
+    
+   
+    <!-- Bootstrap Core JavaScript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script>
+	function poll()
+	{	
+		setTimeout(function(){	
+			$.get('data.html', function(data){			
+				$('#body').html(data);
+			}, 'html');
+			poll();
+		}, 2000);
+	}
+	
+	$(document).ready(function(){
+		$.get('data.html', function(data){			
+			$('#body').html(data);
+		}, 'html');	
+		poll();
+	});
+	
+</script>
+<script src="elements.js" ></script>
+<script src="events.js" ></script>
+
+</body>
+
+</html>
