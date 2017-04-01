f1313911f6a8b5b5d861e1343716b31ea15209bd
dsmith464 <dsmith464@gatech.edu>
2017-04-01 07:07:19 -0500

    update

--- get.php
+++ get.php
@@ -4,4 +4,5 @@
 		$myfile = fopen($_SERVER['DOCUMENT_ROOT']."dev/nsbe/alfred/data.html", "w") or die("Unable to open file!");
 		fwrite($myfile, $_GET['command']);
 		fclose($myfile);
+		echo $_GET['command'];
 	}
