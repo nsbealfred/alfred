9ab82a86658ef3a6846d2cc4c83bd8d9b3b9a2ce
dsmith464 <dsmith464@gatech.edu>
2017-04-01 07:24:36 -0500

    update

--- api.php
+++ api.php
@@ -1,6 +1,6 @@
 <?php
 
 
-echo json_encode(array("speech"=>"Barack Hussein Obama II is the 44th and current President of the United States.", "displayText"=>"Barack Hussein Obama II is the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where "));
+echo json_encode($_POST);
 
 ?>
