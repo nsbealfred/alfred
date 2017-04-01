d94af15c3875a46d79bc1537797ca032b5f7f4de
dsmith464 <dsmith464@gatech.edu>
2017-04-01 07:04:30 -0500

    update

--- get.php
+++ get.php
@@ -1,10 +1,7 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Untitled Document</title>
-</head>
-
-<body>
-</body>
-</html>
+<?php 
+	if(isset($_GET['command']))
+	{
+		$myfile = fopen($_SERVER['DOCUMENT_ROOT']."dev/nsbe/alfred/data.html", "w") or die("Unable to open file!");
+		fwrite($myfile, $_GET['command']);
+		fclose($myfile);
+	}
